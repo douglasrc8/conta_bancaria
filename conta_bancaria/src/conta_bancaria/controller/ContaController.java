0948fd431127da8f977002a27c942314133d61ae
douglas <douglasrc1912@gmail.com>
2023-11-23 16:36:47 -0300

    conta_bancaria

---
+++ conta_bancaria/src/conta_bancaria/controller/ContaController.java
@@ -0,0 +1,96 @@
+
+
+package conta_bancaria.controller;
+
+import java.util.ArrayList;
+
+import conta_bancaria.model.Conta;
+import contaRepository.ContaRepository;
+
+public class ContaController implements ContaRepository {
+
+	// Criar a Collection
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+
+	// Variável para receber o numero da Conta
+	int numero = 0;
+
+	@Override
+	public void procurarPorNumero(int numero) {
+
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null)
+			conta.visualizar();
+		else
+			System.out.println("A Conta número: " + numero + " não foi encontrada!");
+	}
+
+	@Override
+	public void listarTodas() {
+		for (var conta : listaContas) {
+			conta.visualizar();
+		}
+
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		listaContas.add(conta);
+		System.out.println("A Conta numero: " + conta.getNumero() + " foi criada com Sucesso!");
+
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deletar(int numero) {
+
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null)
+			if(listaContas.remove(conta) == true)
+				System.out.println("A Conta numero: " + numero + " foi excluída com sucesso!");
+		else
+			System.out.println("A Conta número: " + numero + " não foi encontrada!");
+
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* Métodos Auxiliares */
+
+	public int gerarNumero() {
+		return ++numero;
+	}
+
+	public Conta buscarNaCollection(int numero) {
+
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero)
+				return conta;
+		}
+
+		return null;
+	}
+}
